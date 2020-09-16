df9d0045031ec1ed587ef13de55837f79f42d8ae
teefy666 <271069556@qq.com>
2020-09-16 14:50:03 +0800

    添加注释

--- src/main/java/com/example/controller/UsersController.java
+++ src/main/java/com/example/controller/UsersController.java
@@ -23,11 +23,22 @@ public class UsersController {
     @Resource
     private UsersService usersServiceImpl;
 
+    /**
+     * 跳转到登录界面
+     * @return
+     */
     @GetMapping("/index")
     public String login() {
         return "login";
     }
 
+    /**
+     * 登录
+     * @param users
+     * @param model
+     * @param session
+     * @return
+     */
     @PostMapping("/login")
     public String userLogin(Users users, Model model, HttpSession session) {
         users.setUpass(MD5Util.getMd5Str(users.getUpass()));
@@ -42,6 +53,11 @@ public class UsersController {
         return "main";
     }
 
+    /**
+     * 登出
+     * @param session
+     * @return
+     */
     @GetMapping("/logout")
     public String userLogout(HttpSession session) {
         session.invalidate();
